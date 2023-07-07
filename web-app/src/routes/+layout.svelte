16a9f71e27e13b89c8d08a7d3d4278e174b9ae90
mg <master-g@users.noreply.github.com>
2023-07-07 11:11:02 +0800

    feat: make it work

--- web-app/src/routes/+layout.svelte
+++ web-app/src/routes/+layout.svelte
@@ -1,4 +1,9 @@
-<script lang='ts'>
+<script lang="ts">
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-modern.css';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 </script>
